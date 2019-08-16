e15f82f4963e5586bb1790e25a63cb7d8440403c
Дмитрий Филиппов <Amoralis.19@gmail.com>
2019-08-16 08:49:10 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -3,82 +3,54 @@ $(document).ready(function () {
 
     if($(window).width() < 1199) {
     $('.services-item').mouseenter(function () {
+      $('a.button').removeClass('serv_active');
       $(this).children('.services-item_overlay').css('width', '200%');
-      $(this).children('button').css('margin-left', '32%').css( 'padding', '20px' ).css('bottom', '60px');
+      $(this).children('a.button').toggleClass('serv_active');
       });
     $('.services-item').mouseleave(function () {
+
       $(this).children('.services-item_overlay').css('width', '90%');
-      $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
+      $(this).children('a.button').toggleClass('serv_active');
+      $('a.button').removeClass('serv_active');
       });
-    }
-    else {
-      $('.services-item').mouseenter(function () {
-        $(this).children('.services-item_overlay').css('width', '200%');
-        $(this).children('button').css('margin-left', '32%').css( 'padding', '20px' ).css('bottom', '60px');
-        });
-      $('.services-item').mouseleave(function () {
-        $(this).children('.services-item_overlay').css('width', '70%');
-        $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
-        });
-    }
-    if($(window).width() < 578) {
-    $('.services-item').mouseenter(function () {
-      $(this).children('.services-item_overlay').css('width', '100%');
-      $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
-      });
-    $('.services-item').mouseleave(function () {
-      $(this).children('.services-item_overlay').css('width', '90%');
-      $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
-      });
-    }
-    else {
-      $('.services-item').mouseenter(function () {
-        $(this).children('.services-item_overlay').css('width', '200%');
-        $(this).children('button').css('margin-left', '32%').css( 'padding', '20px' ).css('bottom', '60px');
-        });
-      $('.services-item').mouseleave(function () {
-        $(this).children('.services-item_overlay').css('width', '70%');
-        $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
-        });
     }
     if($(window).width() < 450) {
     $('.services-item').mouseenter(function () {
-      $(this).children('.services-item_overlay').css('width', '140%');
-      $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
+      $('a.button').removeClass('serv_active');
+      $(this).children('.services-item_overlay').css('width', '200%');
+      $(this).children('a.button').toggleClass('serv_active');
       });
     $('.services-item').mouseleave(function () {
-      $(this).children('.services-item_overlay').css('width', '105%');
-      $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
+      $(this).children('.services-item_overlay').css('width', '115%');
+      $(this).children('a.button').toggleClass('serv_active');
+      $('a.button').removeClass('serv_active');
       });
-    }
-    else {
-      $('.services-item').mouseenter(function () {
-        $(this).children('.services-item_overlay').css('width', '200%');
-        $(this).children('button').css('margin-left', '32%').css( 'padding', '20px' ).css('bottom', '60px');
-        });
-      $('.services-item').mouseleave(function () {
-        $(this).children('.services-item_overlay').css('width', '70%');
-        $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
-        });
     }
     if($(window).width() < 365) {
     $('.services-item').mouseenter(function () {
-      $(this).children('.services-item_overlay').css('width', '140%');
-      $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
+      $('a.button').removeClass('serv_active');
+      $(this).children('.services-item_overlay').css('width', '200%');
+      $(this).children('a.button').toggleClass('serv_active');
       });
     $('.services-item').mouseleave(function () {
       $(this).children('.services-item_overlay').css('width', '125%');
-      $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
+      $(this).children('a.button').toggleClass('serv_active');
+      $('a.button').removeClass('serv_active');
       });
     }
     else {
       $('.services-item').mouseenter(function () {
+        $('a.button').removeClass('serv_active');
         $(this).children('.services-item_overlay').css('width', '200%');
-        $(this).children('button').css('margin-left', '32%').css( 'padding', '20px' ).css('bottom', '60px');
+        $(this).children('a.button').toggleClass('serv_active');
+
         });
       $('.services-item').mouseleave(function () {
         $(this).children('.services-item_overlay').css('width', '70%');
-        $(this).children('button').css('margin-left', '30px').css( 'padding', '15px' ).css('bottom', '20px');
+        $(this).children('a.button').toggleClass('serv_active');
+        $('a.button').removeClass('serv_active');
         });
+        
     }
+    
 });
